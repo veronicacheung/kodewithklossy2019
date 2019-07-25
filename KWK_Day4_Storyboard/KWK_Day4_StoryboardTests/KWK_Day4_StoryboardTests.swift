151dfc5e461afe03fad0036a9f7627ed53c6614f
Apple <apple@Apples-MacBook-Pro-65.local>
2019-07-25 13:09:17 -0400

    tim

---
+++ KWK_Day4_Storyboard/KWK_Day4_StoryboardTests/KWK_Day4_StoryboardTests.swift
@@ -0,0 +1,34 @@
+//
+//  KWK_Day4_StoryboardTests.swift
+//  KWK_Day4_StoryboardTests
+//
+//  Created by Apple on 7/25/19.
+//  Copyright © 2019 Veronica Cheung. All rights reserved.
+//
+
+import XCTest
+@testable import KWK_Day4_Storyboard
+
+class KWK_Day4_StoryboardTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
